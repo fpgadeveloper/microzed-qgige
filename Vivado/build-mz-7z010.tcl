195bb0aeae85368ae5cd151e5bb197af2ed45f4c
Jeff Johnson <jeff@fpgadeveloper.com>
2016-06-19 08:59:00 -0400

    renamed bd scripts to specify MicroZed

--- Vivado/build-mz-7z010.tcl
+++ Vivado/build-mz-7z010.tcl
@@ -92,7 +92,7 @@ current_run -implementation [get_runs impl_1]
 puts "INFO: Project created:mz_7z010_qgige"
 
 # Create block design
-source $origin_dir/src/bd/design_1-7z010.tcl
+source $origin_dir/src/bd/design_1-mz7010.tcl
 
 # Generate the wrapper
 set design_name [get_bd_designs]
